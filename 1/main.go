1e69016dbf2fe50d6f0f5a5bcb0a21d4c57fdb4b
PereverzevIvan <peregh320@gmail.com>
2024-07-13 21:40:09 +0300

    решена задача 1 TwoSum

--- 1/main.go
+++ 1/main.go
@@ -1,13 +1,30 @@
 package main
 
-func twoSums(nums []int, target int) []int {
-	var result []int
-	var hash map[int]int
+import "fmt"
+
+/*
+Дан массив чисел nums и целое число target. Нужно вернуть пару индексов элементов, сумма которых равна target
+Можно считать, что каждый кейс имеет только одно решение.
+*/
+func twoSum(nums []int, target int) []int {
+	result := []int{0, 0}
+	hash := map[int]int{} // Хэш таблица, в которой хранятся числа и индесы
+
+	for i, num := range nums {
+		if j, inMap := hash[target-num]; inMap {
+			result = []int{i, j}
+			break
+		} else {
+			hash[num] = i
+		}
+	}
 
 	return result
 }
 
 func main() {
-
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
+	fmt.Println(twoSum([]int{3, 3}, 6))
 }
 
